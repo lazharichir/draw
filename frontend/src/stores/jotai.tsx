f84226a098c33fd337ae3bdf795c38ee391957c6
Lazhar Ichir <lazhar@topicseed.com>
2023-08-30 19:11:04 +0100

    added brown color

--- frontend/src/stores/jotai.tsx
+++ frontend/src/stores/jotai.tsx
@@ -9,6 +9,7 @@ export const paletteColorsAtom = atomWithImmer<RGBA[]>([
 	{ r: 255, g: 255, b: 0, a: 1 },
 	{ r: 255, g: 0, b: 255, a: 1 },
 	{ r: 0, g: 255, b: 255, a: 1 },
+	{ r: 150, g: 75, b: 0, a: 1 },
 	{ r: 200, g: 200, b: 200, a: 1 },
 	{ r: 100, g: 100, b: 100, a: 1 },
 	{ r: 0, g: 0, b: 0, a: 1 },
